f2c3940d54b88a61a5cd6d5fdc8eb1f69c523493
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2022-11-11 14:02:03 +0000

    inProgress

--- contracts/LinkedList.sol
+++ contracts/LinkedList.sol
@@ -1,31 +1,46 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.7;
 
-contract LinkedList {
+import "hardhat/console.sol";
+
+contract Quest {
     struct Queue {
-        address user;
+        // address user;
         uint256 depositAmount;
         uint256 next;
         uint256 prev;
     }
     mapping(uint256 => Queue) queue;
     uint256 position;
-    uint256 head;
-    uint256 tail;
+    uint256 head = 1;
+    uint256 tail = 0;
 
-    function enqueue(Queue memory data) public {
+    function enqueue(uint amount) public {
+        Queue memory data;
         tail += 1;
-        queue[tail] = data;
+        data.prev = tail;
+        position += 1;
+        uint prev = data.prev - 1;
+        uint next = tail;
+        data = Queue(amount, next, prev);
+        queue[position] = data;
+        // data.prev = tail;
     }
 
     function dequeue() public returns (Queue memory data) {
-        require(tail >= head); // non-empty queue
-
-        data = queue[head];
-
-        delete queue[head];
-        head += 1;
+        if (tail >= head) {
+            // non-empty queue
+            if (data.prev >= data.next) {
+                data = queue[data.next];
+                delete queue[data.next];
+                data.next += 1;
+            }
+            data = queue[head];
+            delete queue[head];
+            head += 1;
+        } else {
+            console.log("No more queue");
+        }
     }
 
     function getData(uint num) public view returns (Queue memory) {
